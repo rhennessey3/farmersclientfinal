d87f1870f9bd6f9e720cc0136bc4f241af1a9a7b
rick <rhennessey3@gmail.com>
2020-11-22 18:33:27 -0500

    changed product string line 8

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function ProductItem(props) {
     return (
         <>
-            <Link to={"/inventory/details/" + props.item.id}>
+            <Link to={`/inventory/details/${props.item.id}`}>
                 <div className="product-item-container">
                     <div className="product-item-photo">
                         <img src="https://picsum.photos/120/120" alt="food" />
