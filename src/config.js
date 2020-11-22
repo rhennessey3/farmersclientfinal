2cd881a368ab44df95713eb0334f55216c8d1fde
rick <rhennessey3@gmail.com>
2020-11-22 18:25:45 -0500

    test

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ let tokenKey;
 if (process.env.NODE_ENV === 'production') {
 
   apiPath = 'https://farmers-bazaar-api.herokuapp.com/api'
-  //tokenKey = '3c02a47f10871c3fe78046a1f6c01a8ad0c3a997152736e3de5ae84baaf9cd2e'
   
 } else {
   apiPath = 'http://localhost:8000/api'
@@ -18,3 +17,6 @@ export  {
   API_ENDPOINT,
   TOKEN_KEY
 }
+
+
+
