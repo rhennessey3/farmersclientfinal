5e3408f43ecda2f10693950e8b167e5b0f48cb4f
MJ <mjstthomas9516@gmail.com>
2020-11-23 11:23:31 -0700

    fixed login and push

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthContext from '../../context/AuthContext'
+import TokenService from '../../services/TokenService'
 import './NavBar.css'
 
 
@@ -35,7 +36,7 @@ export default class NavBar extends Component {
                             {/* <li><Link to="/home">Home Page</Link></li>
                             <li><Link to="/signup">Sign up</Link></li> */}
                             <li><Link to="/add">Add Item</Link></li>
-                            <li><Link to="/login" onClick={() => this.logoutUser()}>{login ? "Login" : "Logout"}</Link></li>
+                            <li><Link to="/login" onClick={() => this.logoutUser()}>{TokenService.hasAuthToken() ? "Logout" : "Login"}</Link></li>
                             {/* <li><Link to="/inventory">Inventory</Link></li>
                             <li><Link to="/details">Product Detail</Link></li> */}
                         </ul>
