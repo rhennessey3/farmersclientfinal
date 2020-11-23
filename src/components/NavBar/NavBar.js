bf779a0a6c279bf35d5831f95c43686eafec64d6
rick <rhennessey3@gmail.com>
2020-11-23 13:16:48 -0500

    fixed nav jam

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthContext from '../../context/AuthContext'
 import './NavBar.css'
+import TokenService from '../services/TokenService';
 
 
 export default class NavBar extends Component {
@@ -31,13 +32,9 @@ export default class NavBar extends Component {
                     <div className="logo"></div>
                     <div className="menu">
                         <ul>
-                            {/* <li><Link to="/">Landing Page</Link></li> */}
-                            {/* <li><Link to="/home">Home Page</Link></li>
-                            <li><Link to="/signup">Sign up</Link></li> */}
                             <li><Link to="/add">Add Item</Link></li>
-                            <li><Link to="/login" onClick={() => this.logoutUser()}>{login ? "Login" : "Logout"}</Link></li>
-                            {/* <li><Link to="/inventory">Inventory</Link></li>
-                            <li><Link to="/details">Product Detail</Link></li> */}
+                            <li><Link to="/login" onClick={() => this.logoutUser()}>{TokenService.hasAuthToken() ? "Login" : "Logout"}</Link></li>
+   
                         </ul>
                     </div>
                 </div>
